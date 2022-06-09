89d5f6b8624696a995bdc96ecb6eabbe5da20b2f
Thi021002 <102248241+Thi021002@users.noreply.github.com>
2022-06-09 16:05:36 -0300

    Arquivos Java

---
+++ src/Projeto_02_Oper_Arit.java
@@ -0,0 +1,17 @@
+public class Projeto_02_Oper_Arit {
+    // metodo main inicia a execução do aplicativo
+    public static void main(String[] args)
+    {
+        System.out.println("Procedência: " + (10/2+3));
+        System.out.println("Alterando a procedência: " + 10/(2+3));
+
+        System.out.println("Divisão Inteira: " + 9/2);
+        System.out.println("Divisão Real: " + 9/2.0);
+        System.out.println("Divisão Real: " + 9.0/2);
+        System.out.println("Divisão Real: " + 9.0/2.0);
+
+        System.out.println("Resto da Divisão Inteira: " + 9%2);
+        System.out.println("Resto da Divisão Inteira: " + 8%2);
+        System.out.println("Resto da Divisão Inteira: " + 9%5);
+    } // fim do método main
+} //fim da classe OperArit
---
+++ src/Projeto_03_Oper_Relac.java
@@ -0,0 +1,10 @@
+public class Projeto_03_Oper_Relac {
+    // Método main inicia a execução do aplicativo
+    public static void main(String[] args)
+    {
+        System.out.println("Teste1: " + (3==2));
+        System.out.println("Teste2: " + (3!=2));
+        System.out.println("Teste3: " + (3>=2));
+        System.out.println("Teste4: " + (3<=2));
+    } // Fim do metodo main
+} // fim da classe
